82b8064840b2d447e5ecb6756ab8c907d750c14f
alex88g <alexander.gallorini@gmail.com>
2025-06-05 20:59:18 +0200

    first commit8

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 //const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
-const API_BASE = import.meta.env.VITE_API_BASE;
+const API_BASE = "/api";
 
 // AUTH
 export const register = (data) => axios.post(`${API_BASE}/auth/register`, data);
