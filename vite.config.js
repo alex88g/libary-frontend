c4c5614698926afd31dc9a9e65c254b94355fffc
alex88g <alexander.gallorini@gmail.com>
2025-06-10 11:35:41 +0200

    vite config updated

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,22 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000', // Din lokala backend
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  }
+export default defineConfig(({ mode }) => {
+  const isDev = mode === 'development';
+
+  return {
+    plugins: [react()],
+    server: isDev
+      ? {
+          port: 5173,
+          proxy: {
+            '/api': {
+              target: 'http://localhost:5000',
+              changeOrigin: true,
+              secure: false
+            }
+          }
+        }
+      : {}
+  };
 });
